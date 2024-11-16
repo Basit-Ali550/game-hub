ee6fca148fff599c16b4ef4b3813aaa9294cab25
Basit ali <chbasitali450@gmail.com>
2024-11-15 18:22:31 -0800

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Button>basit</Button>
+      <Grid
+        templateAreas={{ base: `'nav' 'main'`, lg: `'nav nav' 'aside main'` }}
+      >
+        <GridItem area={"nav"} bg="coral">
+          <Navbar />
+        </GridItem>
+
+        <Show above="lg">
+          <GridItem area={"aside"} bg="gold">
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg="red">
+          main
+        </GridItem>
+      </Grid>
     </>
   );
 }
