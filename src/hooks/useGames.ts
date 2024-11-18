d8733696e7d1d94b6deac9a419d085cd6d8f4fb2
Basit ali <chbasitali450@gmail.com>
2024-11-18 03:14:27 -0800

    Title change

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,7 @@
-// import { useEffect, useState } from "react";
-// import apiClient from "../services/api-client";
 
 import { GameQuery } from "../App";
 import useData from "./uesData";
-// import { CanceledError } from "axios";
+
 export interface Platforms{
   id:number;
   name:string;
@@ -17,33 +15,7 @@ export interface Game {
     metacritic:number;
     rating_top:number
   }
-  // interface FetchGamesResponse {
-  //   count: number;
-  //   results: Game[];
-  // }
-// const useGames=()=>{
-//     const [games, setGames] = useState<Game[]>([]);
-//     const [error, setError] = useState("");
-//     const [isLoading, setisLoading] = useState(false)
-//     useEffect(() => {
-//         const controller=new AbortController()
-//         setisLoading(true)
-//       apiClient
-//         .get<FetchGamesResponse>("/games",{signal:controller.signal})
-//         .then((res) => {
-//           setGames(res.data.results);
-//           setisLoading(false)
-//         })
-//         .catch((err) =>{
-//             if(err instanceof CanceledError) return;
-//             setError(err.message)
-//             setisLoading(false)
-//           });
 
-//         return ()=> controller.abort()
-//     }, []);
-//   return{games,error,isLoading}
-// }
 const useGames =( gameQuery :GameQuery)=>useData<Game>('/games',{params:{
   genres:gameQuery.genre?.id,
   platforms:gameQuery.platform?.id,
