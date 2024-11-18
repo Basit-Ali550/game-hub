ca655fe7bd4592a4a01a9cdedd646c6c13eada9a
Basit ali <chbasitali450@gmail.com>
2024-11-17 20:34:12 -0800

    Build Search Input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SeacrchInput from "./SeacrchInput";
 const Navbar = () => {
   return (
     <HStack padding="10px" justifyContent="space-between">
       <Image src={logo} boxSize="60px" />
+      <SeacrchInput />
       <ColorModeSwitch />
     </HStack>
   );
